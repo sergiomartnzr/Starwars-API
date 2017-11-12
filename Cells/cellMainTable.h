e79e8ceeeea2ae8ab2770356701b48d60d9c9c7b
Sergio Martinez <sergio.martnzr@gmail.com>
2017-11-11 20:32:29 -0600

    - cellMainTable added - cell index and name labels added and registered in cellMainTable.h

---
+++ Cells/cellMainTable.h
@@ -0,0 +1,15 @@
+//
+//  cellMainTable.h
+//  Starwars API
+//
+//  Created by Sergio Martinez on 11/11/17.
+//  Copyright © 2017 Boletomovil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UILabel *lblIndex;
+@property (strong, nonatomic) IBOutlet UILabel *lblName;
+
+@end
---
+++ Cells/cellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  cellMainTable.m
+//  Starwars API
+//
+//  Created by Sergio Martinez on 11/11/17.
+//  Copyright © 2017 Boletomovil. All rights reserved.
+//
+
+#import "cellMainTable.h"
+
+@implementation cellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
